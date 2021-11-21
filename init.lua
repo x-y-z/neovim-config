4fa152a06cb7b0c419a3dfda70c9e756d756aa71
Zi Yan <zi.yan@normal.zone>
2021-11-21 12:47:08 -0500

    first commit
    Signed-off-by: Zi Yan <zi.yan@normal.zone>

---
+++ init.lua
@@ -0,0 +1,31 @@
+--[[
+
+  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+
+
+Neovim init file
+Version: 0.41.0 - 2021/11/19
+Maintainer: Brainf+ck
+Website: https://github.com/brainfucksec/neovim-lua
+
+--]]
+
+-----------------------------------------------------------
+-- Import Lua modules
+-----------------------------------------------------------
+require('plugins/packer')
+require('settings')
+require('keymaps')
+require('plugins/nvim-tree')
+require('plugins/indent-blankline')
+require('plugins/feline')
+require('plugins/vista')
+require('plugins/nvim-cmp')
+require('plugins/nvim-lspconfig')
+require('plugins/nvim-treesitter')
+require('plugins/alpha-nvim')
---
+++ lua/plugins/packer.lua
@@ -0,0 +1,96 @@
+-----------------------------------------------------------
+-- Plugin manager configuration file
+-----------------------------------------------------------
+
+-- Plugin manager: packer.nvim
+-- https://github.com/wbthomason/packer.nvim
+
+-- For information about installed plugins see the README
+--- neovim-lua/README.md
+--- https://github.com/brainfucksec/neovim-lua#readme
+
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+-- Add packages
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- packer can manage itself
+
+  -- file explorer
+  use 'kyazdani42/nvim-tree.lua'
+
+  -- indent line
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- autopair
+  use {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup()
+    end
+  }
+
+  -- icons
+  use 'kyazdani42/nvim-web-devicons'
+
+  -- tagviewer
+  use 'liuchengxu/vista.vim'
+
+  -- treesitter interface
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- colorschemes
+  use 'tanvirtin/monokai.nvim'
+
+  use { 'rose-pine/neovim', as = 'rose-pine' }
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+
+  -- autocomplete
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'saadparwaiz1/cmp_luasnip',
+    },
+  }
+
+  -- statusline
+  use {
+    'famiu/feline.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+  }
+
+  -- git labels
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  -- dashboard
+  use {
+    'goolord/alpha-nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.dashboard'.opts)
+    end
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+
+end)
