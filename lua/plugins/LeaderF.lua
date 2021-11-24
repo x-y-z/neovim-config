c63ec260fce87ea13fb71278bcc382d0c8f2ace7
Zi Yan <zi.yan@normal.zone>
2021-11-23 21:22:31 -0500

    leaderf rg.
    Signed-off-by: Zi Yan <zi.yan@normal.zone>

--- lua/plugins/LeaderF.lua
+++ lua/plugins/LeaderF.lua
@@ -5,7 +5,7 @@ g['Lf_UseCache'] = 0
 g['Lf_UseVersionControlTool'] = 0
 g['Lf_IgnoreCurrentBufferName'] = 1
 g['Lf_StlSeparator'] = { left = "\u{e0b0}", right = "\u{e0b2}", font = "DejaVu Sans Mono for Powerline" }
-g['Lf_PreviewResult'] = {Function = 0, BufTag = 0 }
+g['Lf_PreviewResult'] = {Function = 0, BufTag = 0, Rg = 1 }
 g['Lf_FollowLinks'] = 1
 
 g['Lf_ShortcutF'] = '<leader>ff'
@@ -13,3 +13,5 @@ g['Lf_ShortcutF'] = '<leader>ff'
 g['Lf_GtagsStoreInProject'] = 1
 g['Lf_GtagsAutoGenerate'] = 0
 g['Lf_Gtagslabel'] = 'native-pygments'
+
+g['Lf_RgConfig'] = {"--max-columns=150", "--type-add web:*.{html,css,js}*", "--glob=!git/*", "--hidden"}
