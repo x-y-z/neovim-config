0a157cea05fe20fb459efae5c342307dfe408360
Zi Yan <zi.yan@normal.zone>
2021-11-29 22:07:10 -0500

    add csv.vim.
    Signed-off-by: Zi Yan <zi.yan@normal.zone>

---
+++ lua/plugins/csv.lua
@@ -0,0 +1,4 @@
+local g = vim.g
+
+g['csv_autocmd_arrange'] = 1
+g['csv_autocmd_arrange_size'] = 1024*1024
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -152,6 +152,11 @@ return require('packer').startup(function()
     'tpope/vim-endwise'
   }
 
+  -- fancy csv format
+  use {
+    'chrisbra/csv.vim'
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
