7f11228873d6243fa75df3a73da99a60b5794287
Zi Yan <zi.yan@normal.zone>
2025-01-01 22:37:10 -0500

    replace coc with coq_nvim
    Signed-off-by: Zi Yan <zi.yan@normal.zone>

--- lua/plugins/pluginmanager.lua
+++ lua/plugins/pluginmanager.lua
@@ -139,9 +139,35 @@ return require('lazy').setup({
   'scrooloose/nerdcommenter',
 
   -- language server
-  {
-    'neoclide/coc.nvim',
-    branch = 'release',
+	{
+		"neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+		lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+		dependencies = {
+			-- main one
+			{ "ms-jpq/coq_nvim", branch = "coq" },
+
+			-- 9000+ Snippets
+			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+			-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+			-- Need to **configure separately**
+			{ 'ms-jpq/coq.thirdparty', branch = "3p" }
+			-- - shell repl
+			-- - nvim lua api
+			-- - scientific calculator
+			-- - comment banner
+			-- - etc
+		},
+		init = function()
+			vim.g.coq_settings = {
+					auto_start = true, -- if you want to start COQ at startup
+					-- Your COQ settings here
+			}
+		end,
+		config = function()
+      require'lspconfig'.clangd.setup{}
+			-- Your LSP settings here
+		end,
   },
 
   -- async make or other command
