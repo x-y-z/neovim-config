8db494238128d14753aae33a5f711ea2f574f65e
Zi Yan <zi.yan@normal.zone>
2025-03-30 22:45:49 -0400

    add snacks. change dashboard.
    Signed-off-by: Zi Yan <zi.yan@normal.zone>

--- lua/plugins/pluginmanager.lua
+++ lua/plugins/pluginmanager.lua
@@ -90,15 +90,6 @@ return require('lazy').setup({
     end
   },
 
-  -- dashboard
-  {
-    'goolord/alpha-nvim',
-    dependencies = { 'kyazdani42/nvim-web-devicons' },
-    config = function ()
-      require'alpha'.setup(require'alpha.themes.startify'.opts)
-    end
-  },
-
   {
     'akinsho/bufferline.nvim',
     branch = 'main',
@@ -261,7 +252,44 @@ return require('lazy').setup({
     config = function()
       require('leap').create_default_mappings()
     end
-  }
+  },
+
+  -- snacks
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        enabled = true,
+        example = "files",
+      },
+      bufdelete = {
+        enabled = true,
+      },
+      bigfile = {
+        enabled = true,
+      },
+      lazygit = {
+        enabled = true,
+      }
+    },
+    keys = {
+    --- bufdelete
+      { "bd", function() Snacks.bufdelete() end, desc = "Buffer delete" },
+    --- LSF
+      { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+      { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+      { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+      { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+      { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+      { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+      { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+    --- Others
+      { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    },
+  },
 
 })
 
