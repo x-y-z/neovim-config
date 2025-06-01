98a0fa766cf7b22365cf4a059ee44be6cdb05b8c
Zi Yan <zi.yan@normal.zone>
2025-05-31 22:11:42 -0400

    add nvim-surround remove cscope
    Signed-off-by: Zi Yan <zi.yan@normal.zone>

--- lua/plugins/cscope.lua
+++
@@ -1,9 +0,0 @@
--- load cscope maps
--- pass empty table to setup({}) for default options
-require('cscope_maps').setup({
-  disable_maps = false, -- true disables my keymaps, only :Cscope will be loaded
-  cscope = {
-    db_file = "./cscope.out", -- location of cscope db file
-  },
-})
-
--- lua/plugins/pluginmanager.lua
+++ lua/plugins/pluginmanager.lua
@@ -240,8 +240,16 @@ return require('lazy').setup({
   -- new text surrounding targets, like da, for "ad, sdfads, saf"
   "wellle/targets.vim",
 
-  -- cscope
-  'dhananjaylatkar/cscope_maps.nvim',
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
 
   -- diffview
   'sindrets/diffview.nvim',
